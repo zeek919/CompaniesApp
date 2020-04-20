0ce5b4a5b3b4f1782d5205c7c681413b5f081099
Zeek918 <zeek19988@wp.pl>
2020-04-20 15:17:00 +0200

    Setup project

---
+++ src/containers/Root/Root.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Axios from 'axios';
+
+class Root extends Component {
+    render() {
+        return <div>Setup</div>
+    }
+}
+
+export default Root;
